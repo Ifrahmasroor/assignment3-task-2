618e59fddc72757aabfef960b5876220c64b2e90
ifrah <110095465+Ifrahmasroor@users.noreply.github.com>
2022-08-31 11:16:29 +0500

    Add files via upload

---
+++ task2.js
@@ -0,0 +1,15 @@
+let  age = prompt("Enter  your age:");
+ if(age>=21){
+    console.log("you are confirm entry to a venue and the ability to purshase fire-crackers.");
+
+ }
+ 
+ else if(age<=19)
+ {
+    console.log("confirm entry to the venue but deny  the ability to purshase fire-crackers.");
+    
+ }
+ else{
+    console.log("deny entry ");
+  
+}
